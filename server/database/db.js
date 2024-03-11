315a3e9a0d8487abb05ea84f85d5a1dce5b3ef10
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-03-11 11:19:04 -0300

    created db connection

---
+++ server/database/db.js
@@ -0,0 +1,8 @@
+import dotenv from dotenv;
+import { Sequelize } from 'sequelize';
+const {USERNAME, PASSWORD, HOSTNAME, DATABASE } = process.env
+
+const db = new Sequelize(DATABASE, USERNAME, HOSTNAME || null, {
+    host: HOSTNAME,
+    dialect: 'mysql'
+})
