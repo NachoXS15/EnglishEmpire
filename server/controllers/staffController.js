f4969161a44133cc9ad7a9d38e8071ba4aec30ec
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-03-24 00:11:25 -0300

    completed staff crud

--- server/controllers/staffController.js
+++ server/controllers/staffController.js
@@ -12,3 +12,48 @@ export const getAllStaff = async(req, res) => {
         console.log("Error al traer staff: ", error.message);
     }
 }
+
+export const getOneStaff = async(req, res) => {
+    try {
+        const staff = await staffModel.findOne({where: {id: req.params.id}})
+        console.log(staff)
+        res.json(staff)
+    } catch (error) {
+        console.log("error al traer un coso: ", error.message)
+    }
+}
+
+export const postStaff = async(req, res) => {
+    try {
+        await staffModel.create(req.body)
+        res.json({
+            operation: 'ok'
+        })
+    } catch (error) {
+        console.log("error al crear: ", error.message)
+    }
+}
+
+export const updateStaff = async(req, res) => {
+    try {
+        await staffModel.update(req.body, {where: {id: req.params.id}});
+        res.json({
+            operation: 'ok'
+        })
+    } catch (error) {
+        throw new Error("error al actualizar")
+    }
+}
+
+export const deleteStaff = async(req, res) => {
+    try {
+        await staffModel.destroy({where: {id: req.params.id}})
+        res.json({
+            operation: 'ok'
+        })
+    } catch (error) {
+        throw new Error("error al eliminar")
+    }
+}
+
+
--- server/routes/staffRoutes.js
+++ server/routes/staffRoutes.js
@@ -1,7 +1,11 @@
 import express from 'express'
-import { getAllStaff } from "../controllers/staffController.js";
+import { deleteStaff, getAllStaff, getOneStaff, postStaff, updateStaff } from "../controllers/staffController.js";
 const router = express.Router();
 
 router.get('/', getAllStaff)
+router.get('/:id', getOneStaff)
+router.post('/', postStaff)
+router.put('/:id', updateStaff)
+router.delete('/:id', deleteStaff)
 
 export default router;
