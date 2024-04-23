404a0480aac87ba2c942d367b2f0feab905dd01d
Nicolas Lujan <nico512lujan@gmail.com>
2024-04-23 10:26:28 -0300

    .

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -38,6 +38,10 @@ const router = createBrowserRouter([
   {
     path: '/inscripcion',
     element: <Inscription />
+  },
+  {
+    path: '/inscripcion/:id',
+    element: <Inscription />
   }
 ])
 
