6883c2f9a2fb5ea03cf650b82a2ab1bcac2e7f78
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-05-09 22:11:19 -0300

    status message

--- server/app.js
+++ server/app.js
@@ -1,27 +1,17 @@
-import express from "express";
-import cors from 'cors';
-import db from '../server/database/db.js'
-import staffRouter from "./routes/staffRoutes.js";
-import cursosRouter from './routes/cursosRoutes.js'
-const port = 3000;
+import express from 'express';
+
+import cursosRoutes from './src/routes/cursos.routes.js'
+import staffRoutes from './src/routes/staff.routes.js'
+
 const app = express();
-//permitir origines
-app.use(cors());
-
-//importar enrutadores
+const port = 3000
 app.use(express.json());
-app.use('/staff', staffRouter)
-app.use('/cursos', cursosRouter)
-
-//conexion a db
-try {
-    await db.authenticate();
-    console.log("Conexion correcta")
-} catch (error) {
-    console.log("Error al conectar", error.message)
-}
-
-app.listen(port, () => {
-    console.log(`ejecutando en puerto ${port}`)
+app.use('/api', cursosRoutes)
+app.use('/api', staffRoutes)
+app.use('/', (req, res) => {
+    res.json({
+        estado: 'ok'
+    })
 })
-
+app.listen(port);
+console.log('Server on port: ', `${port}`);
